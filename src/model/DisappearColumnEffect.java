81489a6e715d470c9124e2abb89f8db327c7410b
p1517348 <p1517348@IUTDOUAB2PC132.univ-lyon1.fr>
2016-12-12 11:58:57 +0100

    Ajout des classes (sans code) de l'effet disparition de colonne

---
+++ src/model/DisappearColumnEffect.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ *
+ * @author p1517348
+ */
+public class DisappearColumnEffect extends Effect {
+    
+    /**
+     * 
+     * @param line
+     * @param column
+     * @param game 
+     */
+    @Override
+    public void playEffect(int line, int column, Game game) {
+        
+    }
+}
