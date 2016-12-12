40d598c1150995c5f0bf9e30b602583437760a55
Antonin <antoninguyot98@gmail.com>
2016-12-12 11:57:40 +0100

    fin séance 12/12/2016

---
+++ src/model/DisappearLineEffect.java
@@ -0,0 +1,30 @@
+package model;
+
+public class DisappearLineEffect extends Effect{
+
+	@Override
+	public void playEffect(int line, int column, Game game) {
+		
+		int player1_id = game.getPlayer1().getId();
+        int player2_id = game.getPlayer2().getId();
+		
+		for(int i=0;i<10;i++)
+		{
+			game.getBoard().getTileIJ(line, i).setStatus(-1);
+		}
+		for(int i=0;i<10;i++)
+		{
+			for(int j=line+1;j<10;j++)
+			{
+				int tile_id = game.getBoard().getTileIJ(j, i).getStatus();
+				
+				if(tile_id == player1_id || tile_id == player2_id)
+				{
+					game.getBoard().getTileIJ((j), i).setStatus(-1);
+					game.getBoard().getTileIJ((j-1), i).setStatus(tile_id);
+				}
+			}
+		}
+	}
+
+}
