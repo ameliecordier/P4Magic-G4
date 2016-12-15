8213eb012ece94c945ac01d5b310394a2b3fac34
Léa Vigliano <lea.vigliano@etu.univ-lyon1.fr>
2016-12-15 15:38:56 +0100

    Finalisation de l'effet de disparition de colonne et de son test

--- src/model/DisappearColumnEffect.java
+++ src/model/DisappearColumnEffect.java
@@ -7,13 +7,16 @@ package model;
 public class DisappearColumnEffect extends Effect {
     
     /**
-     * 
+     * This effect delete all the tiles of the column where the tile has just been played
      * @param line
      * @param column
      * @param game 
      */
     @Override
     public void playEffect(int line, int column, Game game) {
-        
+        int height = game.getBoard().getHeight();
+        for(int i=0; i<height-line; i++){
+            game.getBoard().getTileIJ(line+i, column).setStatus(-1);
+        }
     }
 }
--- test/model/DisappearColumnEffectTest.java
+++ test/model/DisappearColumnEffectTest.java
@@ -13,5 +13,148 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DisappearColumnEffectTest {
-    //TODO
+    
+    static Game aGame;
+    
+    public DisappearColumnEffectTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        // Création d'un jeu vide
+        aGame = new Game();
+        Board b = new Board(10, 10);
+        aGame.setBoard(b);
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testDisappearColumnEffectNormalGame() {
+
+        // On pré-remplit le plateau pour les besoins de la simulation 
+        Utils.simulateAGame(aGame);
+
+        // Effet fixé sur une case (qui n'est pas encore remplie)
+        int height = aGame.getBoard().getHeight();
+        // height-3 correspond à la première case vide dans la colonne O, vu que l'on a déjà joué deux coups dans cette colonne
+        aGame.getBoard().getTileIJ(height - 3, 0).setEffect(new DisappearColumnEffect());
+
+        // Récupération de l'ID du joueur avant que le coup soit joué 
+        int id_player = aGame.getCurrentPlayer().getId();
+
+        // Coup joué sur une case ne contenant pas l'effet 
+        aGame.playMove(0);
+
+        // Vérifications :
+        // - l'effet est bien appliqué sur la case 
+        // - le tour de jeu a bien changé
+        // - la colonne dans laquelle le pion est joué est vide
+        assertTrue("Doit être d'effet disappear column", aGame.getBoard().getTileIJ(height - 3, 0).getEffect() instanceof DisappearColumnEffect);
+        assertTrue(aGame.getCurrentPlayer().getId() != id_player);
+        for(int i=2; i>=0;i--) {
+            assertEquals(-1, aGame.getBoard().getTileIJ(height-1-i, 0).getStatus());
+        }
+        
+    }
+
+    /**
+     * Test de DisappearColumnEffect sur grille vide 
+     * Vérification de l'état de la
+     * tuile après application de l'effet 
+     * Résultats attendus : la case doit être
+     * vide, le tour de jeu doit être passé
+     */
+    @Test
+    public void testDisappearColumnEffectEmptyGame() {
+
+        // Effet fixé sur une case 
+        int height = aGame.getBoard().getHeight();
+        aGame.getBoard().getTileIJ(height - 1, 0).setEffect(new DisappearColumnEffect());
+
+        // Récupération de l'ID du joueur courant 
+        int id_player = aGame.getCurrentPlayer().getId();
+
+        // Coup joué sur la case de l'effet 
+        aGame.playMove(0);
+
+        // Vérifications :
+        // - le début de la colonne est bien vide après
+        // - l'effet est bien appliqué sur la case 
+        // - le tour de jeu a bien changé
+        for(int i=2; i>=0;i--) {
+            assertEquals(-1, aGame.getBoard().getTileIJ(height-1-i, 0).getStatus());
+        }
+        assertTrue("Doit être d'effet disappear column", aGame.getBoard().getTileIJ(height - 1, 0).getEffect() instanceof DisappearColumnEffect);
+        assertTrue(aGame.getCurrentPlayer().getId() != id_player);
+
+    }
+
+    /**
+     * Test de DisappearColumnEffect sur grille vide 
+     * Vérification du nombre de jetons
+     * après jeu 
+     * Résultat attendu : le nombre doit être égal à 0
+     */
+    @Test
+    public void testDisappearColumnEffectEmptyGameWithTilesNumber() {
+
+        // Effet fixé sur une case 
+        int height = aGame.getBoard().getHeight();
+        aGame.getBoard().getTileIJ(height - 1, 0).setEffect(new DisappearColumnEffect());
+
+        // Coup joué sur cette case 
+        aGame.playMove(0);
+
+        // Vérification que le nombre de jetons au total est égal à 0  
+        assertEquals(0, aGame.getBoard().getTotalTilesCount());
+
+    }
+
+    /**
+     * Test de DisappearColumnEffect sur grille pré-remplie 
+     * Vérification de l'état de
+     * la tuile après application de l'effet 
+     * Résultat attendu : la case doit
+     * être vide, l'effet doit être sur la case 
+     * et le tour doit être passé
+     */
+    @Test
+    public void testDisappearColumnEffectFilledGame() {
+
+        // On pré-remplit le plateau pour les besoins de la simulation 
+        Utils.simulateAGame(aGame);
+
+        // Effet fixé sur une case (qui n'est pas encore remplie)
+        int height = aGame.getBoard().getHeight();
+        // height-3 correspond à la première case vide dans la colonne O, vu que l'on a déjà joué deux coups dans cette colonne
+        aGame.getBoard().getTileIJ(height - 3, 0).setEffect(new DisappearColumnEffect());
+
+        // Récupération de l'ID du joueur avant que le coup soit joué 
+        int id_player = aGame.getCurrentPlayer().getId();
+
+        // Coup joué sur cette case 
+        aGame.playMove(0);
+
+        // Vérifications :
+        // - le début de la colonne jouée est bien vide après
+        // - l'effet est bien appliqué sur la case 
+        // - le tour de jeu a bien changé
+        for(int i=2; i>=0;i--) {
+            assertEquals(-1, aGame.getBoard().getTileIJ(height-1-i, 0).getStatus());
+        }
+        assertTrue("Doit être d'effet disappear column", aGame.getBoard().getTileIJ(height - 3, 0).getEffect() instanceof DisappearColumnEffect);
+        assertTrue(aGame.getCurrentPlayer().getId() != id_player);
+
+    }
 }
