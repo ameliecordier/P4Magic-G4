959cbd004ca76e8ee6775f9d511bd0d3574afda9
Léa Vigliano <lea.vigliano@etu.univ-lyon1.fr>
2016-12-15 17:58:05 +0100

    Modification de RandomTileEffect (erreur de changement de joueur)

--- src/model/RandomTileEffect.java
+++ src/model/RandomTileEffect.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Random;
+
 /**
  *
  * @author Leen
@@ -8,7 +10,19 @@ public class RandomTileEffect extends Effect {
 
     @Override
     public void playEffect(int line, int column, Game game) {
-        
+        int width = game.getBoard().getWidth();
+        Random rand = new Random();
+        int random = rand.nextInt(width-1);
+        game.playMove(random);
+        Player currentPlayer = game.getCurrentPlayer();
+        Player player1 = game.getPlayer1();
+        Player player2 = game.getPlayer2();
+        if (currentPlayer.getId() == player1.getId()) {
+            currentPlayer = player2;
+        }
+        else {
+            currentPlayer = player1;
+        }
     }
     
 }
