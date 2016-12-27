de33808be8e5607914031f059bf5791fbb223540
Léa Vigliano <lea.vigliano@etu.univ-lyon1.fr>
2016-12-27 19:10:27 +0100

    Modification de RandomTileEffect (erreur précedente corrigée)

--- src/model/RandomTileEffect.java
+++ src/model/RandomTileEffect.java
@@ -10,19 +10,19 @@ public class RandomTileEffect extends Effect {
 
     @Override
     public void playEffect(int line, int column, Game game) {
+        int tile_id = game.getBoard().getTileIJ(line, column).getStatus();
         int width = game.getBoard().getWidth();
+        int height = game.getBoard().getHeight();
+        
         Random rand = new Random();
         int random = rand.nextInt(width-1);
-        game.playMove(random);
-        Player currentPlayer = game.getCurrentPlayer();
-        Player player1 = game.getPlayer1();
-        Player player2 = game.getPlayer2();
-        if (currentPlayer.getId() == player1.getId()) {
-            currentPlayer = player2;
-        }
-        else {
-            currentPlayer = player1;
-        }
+        
+        for (int i=height-1; i>0; i--) {
+            if (game.getBoard().getTileIJ(i,random).getStatus() == -1) {
+                game.getBoard().getTileIJ(i,random).setStatus(tile_id);
+                break;
+               }
+            }
     }
     
 }
