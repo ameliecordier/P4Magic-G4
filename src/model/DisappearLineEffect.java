57ab5df0a68d9a9faf0f3bda2292d78972c3c87c
Antonin <antoninguyot98@gmail.com>
2017-01-04 10:02:49 +0100

    création doc et ajout dernier effet

--- src/model/DisappearLineEffect.java
+++ src/model/DisappearLineEffect.java
@@ -1,7 +1,20 @@
 package model;
 
+/**
+ * MagicP4
+ * IUT Lyon 1 - 2017
+ * @author Antonin-Christian - IUT Lyon 1 - 2017
+ * 
+ */
+
 public class DisappearLineEffect extends Effect{
 
+	/**
+	 * 
+	 * @param line
+     * @param column
+     * @param game 
+	 */
 	@Override
 	public void playEffect(int line, int column, Game game) {
 		
@@ -9,10 +22,12 @@ public class DisappearLineEffect extends Effect{
 		int player1_id = game.getPlayer1().getId();
         int player2_id = game.getPlayer2().getId();
 		
+        //On fait disparaitre tous les cube de la ligne
 		for(int i=0;i<10;i++)
 		{
 			game.getBoard().getTileIJ(line, i).setStatus(-1);
 		}
+		//puis on replace tous les cubes correctement en les faisant aller vers le bas s'il n'y a plus rien en dessous d'eux
 		for(int i=0;i<10;i++)
 		{
 			for(int j=line;j>=0;j--)
