e56317501377c71ab7865eed1f9e01e8e87806c3
Fonteix <EtiX42@wu-pc-01989.univ-lyon1.fr>
2017-01-05 16:16:29 +0100

    Ajout DisappearEffect

--- src/model/DisappearEffect.java
+++ src/model/DisappearEffect.java
@@ -9,13 +9,12 @@ package model;
  * @author hakkahi / acordier - IUT Lyon 1 - 2016
  */
 public class DisappearEffect extends Effect {
-
-    /**
-     * TODO
-     */
+    
+    
     @Override
     public void playEffect(int line, int column, Game game) {
-
+        int tile_id = -1;
+        game.getBoard().getTileIJ(line, column).setStatus(tile_id);
     }
 
 }
