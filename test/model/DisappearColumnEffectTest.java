c0833610773573356a3ebd5e7f68dd973d79342d
p1517700 <p1517700@IUTDOUAB2PC72.univ-lyon1.fr>
2017-06-13 09:35:50 +0200

    Tests DisappearColumEffect

--- test/model/DisappearColumnEffectTest.java
+++ test/model/DisappearColumnEffectTest.java
@@ -4,7 +4,8 @@
  */
 package model;
 
-import static model.ChangeColorEffectTest.aGame;
+import static model.DisappearColumnEffectTest.aGame;
+import static model.DisappearEffectTest.aGame;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -47,8 +48,34 @@ public class DisappearColumnEffectTest {
      * Test of playEffect method, of class DisappearColumnEffect.
      */
     @Test
-    public void testChangeColorEffectNormalGame() {
+    public void testDisappearColumnEffectNormalGame() {
+        // On pré-remplit le plateau pour les besoins de la simulation 
+        Utils.simulateAGame(aGame);
 
+        // Effet fixé sur une case (qui n'est pas encore remplie)
+        int height = aGame.getBoard().getHeight();
+        // height-3 correspond à la première case vide dans la colonne O, vu que l'on a déjà joué deux coups dans cette colonne
+        aGame.getBoard().getTileIJ(height - 3, 0).setEffect(new DisappearColumnEffect());
+
+        // Récupération de l'ID du joueur avant que le coup soit joué 
+        int id_player = aGame.getCurrentPlayer().getId();
+
+        // Récupération du nombre de pions présents 
+        int nb_tokens_before = aGame.getBoard().getTotalTilesCount();
+
+        // Coup joué sur une case ne contenant pas l'effet 
+        aGame.playMove(1);
+
+        // Récupération du nombre de pions après le coup 
+        int nb_tokens_after = aGame.getBoard().getTotalTilesCount();
+
+        // Vérifications :
+        // - l'effet est bien appliqué sur la case 
+        // - le tour de jeu a bien changé
+        // - il y a bien un pion de plus sur le plateau
+        assertTrue("Doit être d'effet disappearColumn", aGame.getBoard().getTileIJ(height - 3, 0).getEffect() instanceof DisappearColumnEffect);
+        assertTrue(aGame.getCurrentPlayer().getId() != id_player);
+        assertEquals(nb_tokens_before + 1, nb_tokens_after);
         
     }
 
@@ -56,40 +83,71 @@ public class DisappearColumnEffectTest {
      * Test 
      */
     @Test
-    public void testChangeColorEffectEmptyGame() {
-
+    public void testDisappearColumnEffectEmptyGame() {
         
+        // Effet fixé sur une case 
+        int height = aGame.getBoard().getHeight();
+        aGame.getBoard().getTileIJ(height - 1, 0).setEffect(new DisappearColumnEffect());
 
+        // Récupération de l'ID du joueur courant 
+        int id_player = aGame.getCurrentPlayer().getId();
+
+        // Coup joué sur la case de l'effet 
+        aGame.playMove(0);
+
+        // Vérifications :
+        // - la case est bien vide après
+        // - l'effet est bien appliqué sur la case 
+        // - le tour de jeu a bien changé
+        assertEquals(-1, aGame.getBoard().getTileIJ(height - 1, 0).getStatus());
+        assertTrue("Doit être d'effet disappearColumn", aGame.getBoard().getTileIJ(height - 1, 0).getEffect() instanceof DisappearColumnEffect);
+        assertTrue(aGame.getCurrentPlayer().getId() != id_player);
+        
     }
     
     /**
      * Test 
      */
     @Test
-    public void testChangeColorEffectTwoTokensEmptyGame() {
+    public void testDisappearColumnEffectEmptyGameWithTilesNumber() {
+        // Effet fixé sur une case 
+        int height = aGame.getBoard().getHeight();
+        aGame.getBoard().getTileIJ(height - 1, 0).setEffect(new DisappearColumnEffect());
 
+        // Coup joué sur cette case 
+        aGame.playMove(0);
+
+        // Vérification que le nombre de jetons au total est égal à 0  
+        assertEquals(0, aGame.getBoard().getTotalTilesCount());
         
-
     }
 
     /**
      * Test 
      */
     @Test
-    public void testChangeColorEffectEmptyGameWithTilesNumber() {
+    public void testDisappearColumnEffectFilledGame() {
+        // On pré-remplit le plateau pour les besoins de la simulation 
+        Utils.simulateAGame(aGame);
 
-        
+        // Effet fixé sur une case (qui n'est pas encore remplie)
+        int height = aGame.getBoard().getHeight();
+        // height-3 correspond à la première case vide dans la colonne O, vu que l'on a déjà joué deux coups dans cette colonne
+        aGame.getBoard().getTileIJ(height - 3, 0).setEffect(new DisappearColumnEffect());
+
+        // Récupération de l'ID du joueur avant que le coup soit joué 
+        int id_player = aGame.getCurrentPlayer().getId();
+
+        // Coup joué sur cette case 
+        aGame.playMove(0);
+
+        // Vérifications :
+        // - la case est bien vide après
+        // - l'effet est bien appliqué sur la case 
+        // - le tour de jeu a bien changé
+        assertEquals(-1, aGame.getBoard().getTileIJ(height - 3, 0).getStatus());
+        assertTrue("Doit être d'effet disappearColumn", aGame.getBoard().getTileIJ(height - 3, 0).getEffect() instanceof DisappearColumnEffect);
+        assertTrue(aGame.getCurrentPlayer().getId() != id_player);
 
     }
-
-    /**
-     * Test 
-     */
-    @Test
-    public void testChangeColorEffectFilledGame() {
-
-        
-
-    }
-    
 }
