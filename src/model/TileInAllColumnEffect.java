c8ddee297e4e1937bb6a5024cfa00072fbe1a21e
p1517700 <p1517700@IUTDOUAB2PC72.univ-lyon1.fr>
2017-06-16 10:19:59 +0200

    Implemenation TileInAllColumnEffect

--- src/model/TileInAllColumnEffect.java
+++ src/model/TileInAllColumnEffect.java
@@ -5,8 +5,25 @@ package model;
  * @author p1517700
  */
 public class TileInAllColumnEffect extends Effect{
+    /**
+     * This effect play a tile of current color in all columns
+     * 
+     * @param line
+     * @param column
+     * @param game
+     */
     @Override
     public void playEffect(int line, int column, Game game) {
-        
+        int tile_id = game.getBoard().getTileIJ(line, column).getStatus();
+        int width = game.getBoard().getWidth();
+        for(int i = 0 ; i < width ; i++) {
+            if(column != i) {
+                int j = game.getBoard().getHeight()-1;
+                while(game.getBoard().getTileIJ(j, i).getStatus() != -1) {
+                    j--;
+                }
+                game.getBoard().getTileIJ(j, i).setStatus(tile_id);
+            }
+        }
     }
 }
