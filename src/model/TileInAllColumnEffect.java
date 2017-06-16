b5c4e1584dc534be7f587d322410d3342eb116f7
p1517700 <p1517700@IUTDOUAB2PC72.univ-lyon1.fr>
2017-06-16 10:15:26 +0200

    Tests TileInAllColumn

---
+++ src/model/TileInAllColumnEffect.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ *
+ * @author p1517700
+ */
+public class TileInAllColumnEffect extends Effect{
+    @Override
+    public void playEffect(int line, int column, Game game) {
+        
+    }
+}
